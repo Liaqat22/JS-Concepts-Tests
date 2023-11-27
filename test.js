06b4a9fabf9340494edc137caf559fd414dc8c6a
Liaqat Ali <liaqatali097306@gmail.com>
2023-11-27 20:50:20 +0500

    Arrays

---
+++ test.js
@@ -0,0 +1,41 @@
+// ReverseArray
+const reverseary =(ary)=>{
+  const aryget = []
+for(let i = ary.length-1 ; i>0 ; i--){
+  const data = ary[i]
+  aryget.push(data)
+}
+return aryget
+}
+const  num = [1,2,4,6,9,55] 
+console.log(reverseary(num) , "reverse") //[ 55, 9, 6, 4, 2 ] reverse
+
+// WordCounter
+const Wordcounter = (str) =>{
+  const word = str.split(" ")
+  const words = word.reverse().join(" ") //ali liaqat am i hi
+  console.log(words)
+  return words.length
+}
+const string = "hi i am liaqat ali"
+console.log(Wordcounter(string)) //5
+
+//Removing duplicates 
+function RemoveDplts(arry){
+  const res = arry.filter((value , index)=>{
+    return arry.indexOf(value) === index
+  })
+  console.log(res)
+}
+const arry = [1,2,3,4,5,8,4,4,6]
+RemoveDplts(arry)
+
+//Adding newvalue in Arry
+const newArray = (arry ,newvalue) =>{
+const newentry = [...arry,newvalue]
+return newentry
+}
+const value = [22,32,54545,656,66]
+const newvalue = 100
+console.log(newArray(value,newvalue))
+
